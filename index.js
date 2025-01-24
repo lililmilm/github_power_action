0c9b81d25299308568e142566da16cf712c18a2a
lililmilm <168443118+lililmilm@users.noreply.github.com>
2025-01-24 17:50:06 +0900

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 async function makeIssue() {
     const token = process.env.GH_TOKEN; // 주의! 일치해야함 (env와 일치해야함)
     // 요고 1
-    const OWNER = "qus0in"; // 여러분들의 github 계정 이름
+    const OWNER = "lililmilm"; // 여러분들의 github 계정 이름
     // 요고 2
-    const REPO = "github_power_actions"; // 여러분들의 현재 리포지터리 이름
+    const REPO = "github_power_action"; // 여러분들의 현재 리포지터리 이름
     const response = await fetch(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
         method: 'POST',
         headers: {
             Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
-            title: "행운의 숫자", // 요고 3
+            title: "오늘 운동 시간", // 요고 3
             body: `${Math.floor(Math.random() * 100) + 1}`, // 요고 4
         })
     });
